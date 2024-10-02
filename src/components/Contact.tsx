cc08b16cc928b6c8ad4d9eacd8a8a08d6c7fcd7e
Deyan <deyan.vukic@epitech.eu>
2024-10-02 11:28:13 +0200

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -15,42 +15,6 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 
 export function Contact() {
-    const [email, setEmail] = useState('');
-    const [subject, setSubject] = useState('');
-    const [message, setMessage] = useState('');
-    const [company, setCompany] = useState('');
-    const [status, setStatus] = useState('');
-
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-
-        try {
-            const response = await fetch('/api/send-email', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    to: email,
-                    subject,
-                    text: message,
-                    company, // Optionnel
-                }),
-            });
-
-            if (response.ok) {
-                setStatus('Message envoyé avec succès !');
-                setEmail('');
-                setSubject('');
-                setMessage('');
-                setCompany(''); // Réinitialiser si nécessaire
-            } else {
-                setStatus('Erreur lors de l\'envoi du message.');
-            }
-        } catch (error) {
-            setStatus('Erreur lors de l\'envoi du message.');
-        }
-    };
 
     return (
         <Card className="widthCard m-auto mt-20" id='contact'>
@@ -68,8 +32,6 @@ export function Contact() {
                             <Input
                                 type="email"
                                 id="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
                                 placeholder="example@email.com"
                                 required
                             />
@@ -78,8 +40,6 @@ export function Contact() {
                             <Label htmlFor="company">Entreprise (facultatif)</Label>
                             <Input
                                 id="company"
-                                value={company}
-                                onChange={(e) => setCompany(e.target.value)}
                                 placeholder="Nom entreprise"
                             />
                         </div>
@@ -88,8 +48,6 @@ export function Contact() {
                         <Label htmlFor="subject">Objet</Label>
                         <Input
                             id="subject"
-                            value={subject}
-                            onChange={(e) => setSubject(e.target.value)}
                             placeholder="Objet du message"
                             required
                         />
@@ -98,8 +56,6 @@ export function Contact() {
                         <Label htmlFor="message">Message</Label>
                         <Textarea
                             id="message"
-                            value={message}
-                            onChange={(e) => setMessage(e.target.value)}
                             placeholder="Votre message ..."
                             rows={5}
                             required
